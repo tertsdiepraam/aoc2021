889ac34894b7ef20782fda7d12dbccb8112da464
Terts Diepraam <terts.diepraam@gmail.com>
2021-12-02 11:07:30 +0100

    day 2: small refactor

--- src/two.rs
+++ src/two.rs
@@ -1,21 +1,18 @@
 const INPUT: &'static str = include_str!("../input/2.txt");
 
-fn parse() -> Vec<(&'static str, i32)> {
+fn parse() -> impl Iterator<Item = (&'static str, i32)> {
     INPUT
         .lines()
         .flat_map(|s| s.split_once(' '))
-        .map(|(d, n)| (d, n.parse().unwrap()))
-        .collect()
+        .flat_map(|(d, n)| Some((d, n.parse().ok()?)))
 }
 
 pub fn first() -> i32 {
     let (x, y) = parse()
-        .into_iter()
-        .map(|(d, n): (_, i32)| match d {
-            "forward" => (n, 0),
+        .map(|(d, n)| match d {
             "up" => (0, -n),
             "down" => (0, n),
-            _ => (0, 0),
+            _ => (n, 0),
         })
         .reduce(|(a, b), (x, y)| (a + x, b + y))
         .unwrap();
@@ -23,13 +20,10 @@ pub fn first() -> i32 {
 }
 
 pub fn second() -> i32 {
-    let (x, y, _) = parse()
-        .into_iter()
-        .fold((0, 0, 0), |(x, y, a), (d, n): (_, i32)| match d {
-            "forward" => (x + n, y + a * n, a),
-            "up" => (x, y, a - n),
-            "down" => (x, y, a + n),
-            _ => (x, y, a),
-        });
+    let (x, y, _) = parse().fold((0, 0, 0), |(x, y, a), (d, n)| match d {
+        "up" => (x, y, a - n),
+        "down" => (x, y, a + n),
+        _ => (x + n, y + a * n, a),
+    });
     x * y
 }
