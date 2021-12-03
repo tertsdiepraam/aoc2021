725ec090827701d4aed03be9c41c3cb5cf1d9ed4
Terts Diepraam <terts.diepraam@gmail.com>
2021-12-03 13:22:07 +0100

    list of days instead of range

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of Code 2021
+
+```shell
+# run all exercises
+cargo run
+
+# run specific exercises
+cargo run [DAYS...]
+```
+
+And remember, `--release` make Rust go brrrrrr
--- src/main.rs
+++ src/main.rs
@@ -7,24 +7,19 @@ exercises!();
 fn main() -> Result<(), ParseIntError> {
     let args: Vec<String> = env::args().collect();
 
-    let (from, to) = match &args[1..] {
-        [] => (0usize, EXERCISES.len() / 2),
-        [x] => {
-            let x = x.parse()?;
-            (x - 1, x)
-        }
-        [a, b] => (a.parse::<usize>()? - 1, b.parse::<usize>()?),
-        _ => panic!(),
+    let is: Vec<_> = match &args[1..] {
+        [] => (1usize..=(EXERCISES.len()/2)).collect(),
+        xs => xs.iter().map(|x| x.parse()).collect::<Result<_, _>>()?,
     };
 
     let now = Instant::now();
-    let answers: Vec<i32> = EXERCISES[2 * from..2 * to].iter().map(|e| e()).collect();
+    let answers: Vec<_> = is.iter().map(|i| (i, EXERCISES[2*i-2](), EXERCISES[2*i-1]())).collect();
     let time = now.elapsed().as_micros();
 
-    for (i, a) in answers.chunks(2).enumerate() {
-        println!("Day {}", i + 1);
-        println!("  Part 1: {}", a[0]);
-        println!("  Part 2: {}", a[1]);
+    for (i, a, b) in answers {
+        println!("Day {}", i);
+        println!("  Part 1: {}", a);
+        println!("  Part 2: {}", b);
         println!();
     }
     println!("Time: {}μs", time);
