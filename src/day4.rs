8375c69b4ffa98b7c00b63295ea7ac656b952db5
Terts Diepraam <terts.diepraam@gmail.com>
2021-12-04 16:44:35 +0100

    day 4: optimalisation

--- src/day4.rs
+++ src/day4.rs
@@ -1,14 +1,12 @@
 const INPUT: &'static str = include_str!("../input/4.txt");
+const CARD_SIZE: usize = 5;
 
-type Card = [[Field; 5]; 5];
-
-#[derive(Default, Debug, Clone, Copy)]
-struct Field {
-    n: i32,
-    crossed: bool,
+struct Card {
+    fields: [usize; CARD_SIZE * CARD_SIZE],
+    sums: [usize; 2 * CARD_SIZE],
 }
 
-fn parse() -> (Vec<i32>, Vec<Card>) {
+fn parse() -> (Vec<usize>, Vec<Card>) {
     let mut lines = INPUT.lines();
     let numbers = lines
         .next()
@@ -18,17 +16,21 @@ fn parse() -> (Vec<i32>, Vec<Card>) {
         .collect();
 
     let mut cards = Vec::new();
-    // The whitespace
+
     while lines.next().is_some() {
-        let mut card = [[Field::default(); 5]; 5];
-        for i in 0..5 {
-            let line = lines.next().unwrap();
-            dbg!(line);
-            for (j, val) in line.split(' ').flat_map(str::parse).enumerate() {
-                card[i][j].n = val;
-            }
+        let mut fields = [0; CARD_SIZE * CARD_SIZE];
+        let mut sums = [0; 2 * CARD_SIZE];
+        for (i, val) in lines
+            .by_ref()
+            .take(5)
+            .flat_map(|l| l.split_ascii_whitespace().flat_map(str::parse))
+            .enumerate()
+        {
+            fields[i] = val;
+            sums[i / CARD_SIZE] += val;
+            sums[CARD_SIZE + i % CARD_SIZE] += val;
         }
-        cards.push(card);
+        cards.push(Card { fields, sums });
     }
 
     (numbers, cards)
@@ -38,22 +40,14 @@ pub fn first() -> i32 {
     let (numbers, mut cards) = parse();
     for n in numbers {
         for c in &mut cards {
-            for row in c {
-                for field in row {
-                    if field.n == n {
-                        field.crossed = true;
-                    }
-                }
-            }
-        }
-        for c in &cards {
-            for i in 0..5 {
-                if c[i].iter().all(|f| f.crossed) || c.iter().map(|row| row[i]).all(|f| f.crossed) {
-                    let total: i32 = c
-                        .iter()
-                        .flat_map(|r| r.map(|f| if f.crossed { 0 } else { f.n }))
-                        .sum();
-                    return total * n;
+            if let Some(i) = c.fields.iter().position(|&f| f == n) {
+                let (x, y) = (i / CARD_SIZE, CARD_SIZE + (i % CARD_SIZE));
+
+                c.sums[x] -= n;
+                c.sums[y] -= n;
+
+                if c.sums[x] * c.sums[y] == 0 {
+                    return (c.sums[..CARD_SIZE].iter().sum::<usize>() * n) as i32;
                 }
             }
         }
@@ -63,39 +57,19 @@ pub fn first() -> i32 {
 
 pub fn second() -> i32 {
     let (numbers, mut cards) = parse();
-    for n in numbers {
-        for c in &mut cards {
-            for row in c {
-                for field in row {
-                    if field.n == n {
-                        field.crossed = true;
-                    }
+    cards.iter_mut().map(|c| {
+        for (k, n) in numbers.iter().enumerate() {
+            if let Some(i) = c.fields.iter().position(|f| f == n) {
+                let (x, y) = (i / CARD_SIZE, CARD_SIZE + (i % CARD_SIZE));
+
+                c.sums[x] -= n;
+                c.sums[y] -= n;
+
+                if c.sums[x] * c.sums[y] == 0 {
+                    return (k, (c.sums[..CARD_SIZE].iter().sum::<usize>() * n) as i32);
                 }
             }
         }
-        if cards.len() > 1 {
-            cards.retain(|c| {
-                for i in 0..5 {
-                    if c[i].iter().all(|f| f.crossed)
-                        || c.iter().map(|row| row[i]).all(|f| f.crossed)
-                    {
-                        return false;
-                    }
-                }
-                true
-            });
-        } else {
-            let c = cards[0];
-            for i in 0..5 {
-                if c[i].iter().all(|f| f.crossed) || c.iter().map(|row| row[i]).all(|f| f.crossed) {
-                    let total: i32 = cards[0]
-                        .iter()
-                        .flat_map(|r| r.map(|f| if f.crossed { 0 } else { f.n }))
-                        .sum();
-                    return total * n;
-                }
-            }
-        }
-    }
-    0
+        (0,0)
+    }).max_by_key(|t| t.0).unwrap_or((0,0)).1
 }
